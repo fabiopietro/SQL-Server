df536219587e3f8fa847b7dac086a17494c1e579
Fábio Pietro <67837105+fabiopietro@users.noreply.github.com>
2020-07-20 18:51:17 -0300

    Add files via upload
    Script para listas todas as tabelas de todos o bancos com numero de linhas, espaço usado, livre e total

---
+++ ListaTabelasTodosOsBancos.sql
@@ -0,0 +1,34 @@
+DECLARE  @RESULTADO AS TABLE    (
+                                 Banco                SYSNAME
+                                ,Tabela               SYSNAME
+                                ,Linhas               BIGINT
+                                ,EspacoTotal          BIGINT         
+                                ,EspacoUsadoKB        BIGINT                    
+                                ,EspacoNaoUsadoKB     BIGINT     
+                                )
+INSERT @RESULTADO  ( Banco, Tabela, Linhas, EspacoTotal, EspacoUsadoKB, EspacoNaoUsadoKB ) 
+EXEC sp_msforeachdb '
+IF (''?'' NOT IN (''master'', ''msdb'', ''model'', ''tempdb'', ''DBA_BACKUP'',''DBAdmin'',''DBAVIVO''))
+BEGIN
+
+    SELECT "?" AS db
+            ,name 
+            ,p.rows                                              AS Linhas
+            ,SUM(a.total_pages) * 8                              AS EspacoTotal
+            ,SUM(a.used_pages) * 8                               AS EspacoUsadoKB
+            ,SUM(a.total_pages)  * 8 - SUM(a.used_pages) * 8     AS EspacoUsadoKB
+           
+    FROM [?].sys.tables                 t
+    INNER JOIN [?].sys.partitions       p ON p.object_id    = t.OBJECT_ID 
+    INNER JOIN [?].sys.allocation_units a ON p.partition_id = a.container_id
+
+    GROUP BY t.Name
+            ,p.Rows
+
+END
+'
+
+SELECT * 
+FROM @RESULTADO
+order by 1
+go
